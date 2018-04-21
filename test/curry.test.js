e935268c55d43abed1c8de8ba3c1910fc85d9849
Victor Perin <me@victorperin.ninja>
2018-04-20 21:07:09 -0300

    tests(curry): should execute { a }{ b }, if object params function

--- test/curry.test.js
+++ test/curry.test.js
@@ -1,11 +1,29 @@
 describe('Named curry', () => {
   const namedCurry = require('../src/index')
 
-  it('should curry { a, b }', () => {
-    const func = ({ a, b }) => a + b
+  it('should execute { a, b }', () => {
+    const func = (a, b) => a + b
     const curriedFunc = namedCurry(func)
 
-    const result = func({ a: 1 })({ b: 2 })
+    const result = curriedFunc({ a: 1, b: 2 })
+
+    expect(result).toBe(3)
+  })
+
+  it('should execute { a }{ b }', () => {
+    const func = (a, b) => a + b
+    const curriedFunc = namedCurry(func)
+
+    const result = curriedFunc({ a: 1 })({ b: 2 })
+
+    expect(result).toBe(3)
+  })
+
+  it('should execute { a }{ b }, if object params function', () => {
+    const func = ({a, b}) => a + b
+    const curriedFunc = namedCurry(['a', 'b'], func)
+
+    const result = curriedFunc({ a: 1 })({ b: 2 })
 
     expect(result).toBe(3)
   })
